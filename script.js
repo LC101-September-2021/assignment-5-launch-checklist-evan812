d072b609b6e3b5bd8a007c1f0212f61ac59ae99b
evan812 <61596243+evan812@users.noreply.github.com>
2021-12-13 19:26:39 -0600

    Finished product

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+// const { addDestinationInfo } = require("./scriptHelper");
 
+
+
+window.addEventListener("load", function() {
+    
+    formSubmission(document);
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch(); //response.json() 
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        let planet = listedPlanets[pickPlanet(listedPlanets)]; //Math.random
+        
+        addDestinationInfo(document, planet.name, planet.diameter, planet.moons, planet.star, planet.distance, planet.image); //meat and potatoes
    })
    
 });
